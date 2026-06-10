e01e759dc4c1948159b77abe3079422a4112a41f
Sammi-Rae <samantha.claibourn@gmail.com>
2026-06-10 11:18:25 -0500

    Demos and Input Examples

---
+++ demo_code/for_loop_demo.py
@@ -0,0 +1,17 @@
+def main():
+    # Print integers from 0 and 10
+    for number in range(11):
+        print(number)
+
+# Print integers from 5 and 10
+    print("\n\n")
+    for number in range(5, 11):
+        print(number)
+
+# Print even integers from 0 and 10
+    print("\n\n")
+    for number in range(0, 11, 2):
+        print(number)
+
+
+main()
